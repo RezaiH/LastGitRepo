a2817e94ad946a1b194c86afa3edc19353e16469
Hussain Rezai <rezaih637@gmail.com>
2022-11-13 23:19:27 -0500

    Forth commit

--- src/first/pack/TestingGitRemtoe.java
+++ src/first/pack/TestingGitRemtoe.java
@@ -2,4 +2,7 @@ package first.pack;
 
 public class TestingGitRemtoe {
 
+	public static void main(String[] args) {
+		System.out.println("Is this class pushed to the remote repository(FinalBranch)?");
+	}
 }
