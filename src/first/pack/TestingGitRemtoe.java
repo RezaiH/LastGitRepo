0e77bca86ad4e514eee74b0c1b12e00c23dd916c
Hussain Rezai <rezaih637@gmail.com>
2022-11-13 23:21:21 -0500

    Sixth commit

--- src/first/pack/TestingGitRemtoe.java
+++ src/first/pack/TestingGitRemtoe.java
@@ -5,4 +5,8 @@ public class TestingGitRemtoe {
 	public static void main(String[] args) {
 		System.out.println("Is this class pushed to the remote repository(FinalBranch)?");
 	}
+	
+	private static void isChangeGood() {
+		System.out.println("Yes, change is always good!");
+	}
 }
