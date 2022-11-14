8a8cd60a35bff4b57cff5b9f11e8f55324ca01ba
Hussain Rezai <rezaih637@gmail.com>
2022-11-13 23:15:32 -0500

    Secon commit

--- src/first/pack/Test_Git.java
+++ src/first/pack/Test_Git.java
@@ -5,4 +5,8 @@ public class Test_Git {
 	public static void main(String [] args) {
 		System.out.println("This is just a test!");
 	}
+	
+	public static void showTest() {
+		System.out.println("this is the second test!");
+	}
 }
