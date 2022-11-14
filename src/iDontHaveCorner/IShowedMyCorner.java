af6413e3d7562d4debab781d3314c2e51d8e2906
Hussain Rezai <rezaih637@gmail.com>
2022-11-13 23:43:15 -0500

    lasting commit

--- src/iDontHaveCorner/IShowedMyCorner.java
+++ src/iDontHaveCorner/IShowedMyCorner.java
@@ -5,4 +5,12 @@ public class IShowedMyCorner {
 	public static void main(String[] args) {
 		System.out.println("this is my corner!");
 	}
+	
+	public static void whereIsYourHome() {
+		System.out.println("I am homeless!");
+	}
+	
+	public static void whatIsYourJob() {
+		System.out.println("I am a gang member!");
+	}
 }
--- src/showYourCorner/SpendSomeMoney.java
+++ src/showYourCorner/SpendSomeMoney.java
@@ -2,4 +2,8 @@ package showYourCorner;
 
 public class SpendSomeMoney {
 
+	public static void main(String[] args) {
+		System.out.println("How are you today?");
+	}
+	
 }
